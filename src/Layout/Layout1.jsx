363cf60985f786f8aa32d8a30aa914b031fd678d
Muhammad Uzair <149560339+ItxUzair1@users.noreply.github.com>
2024-11-28 13:29:06 +0500

    Update and rename Layout.jsx to Layout1.jsx

--- src/Layout/Layout1.jsx
+++ src/Layout/Layout1.jsx
@@ -2,7 +2,7 @@ import Footer from "@/components/MyComponents/Footer";
 import Header from "@/components/MyComponents/Header";
 import { Outlet } from "react-router-dom";
 
-export default function Layout(){
+export default function Layout1(){
     return (
         <div>
             <Header/>
